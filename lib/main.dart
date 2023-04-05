6fdc0081d418b22a238ef9e179741118d79d6c3d
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-05 15:25:37 -0600

    remover warnings

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:calculadora/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
 
  
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
  
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
